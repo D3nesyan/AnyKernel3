b85b5a336457e8e2ccd9ecbb914a707ca10c428e
Ratoriku <a1063021545@gmail.com>
2020-03-28 21:57:04 +0800

    Init extra for Spectrum
    Signed-off-by: Ratoriku <a1063021545@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -31,6 +31,12 @@ ramdisk_compression=auto;
 set_perm_recursive 0 0 755 644 $ramdisk/*;
 set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
 
+# Extra
+mount -o rw,remount -t auto /vendor;
+chattr -R -i /vendor/etc/init/hw/;
+cp -rf $home/extra/* /vendor/etc/init/hw/;
+chmod -R 0644 /vendor/etc/init/hw/*;
+chattr -R +i /vendor/etc/init/hw/;
 
 ## AnyKernel install
 dump_boot;
