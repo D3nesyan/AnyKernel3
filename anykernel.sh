80cff245d268a830ad8be151d500a224e2cb3620
Ratoriku <a1063021545@gmail.com>
2020-06-09 12:04:06 +0800

    .

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=ShadowBoarder Kernel by Ratoriku@github.com
+kernel.string=PaperMoon Kernel by Ratoriku@github.com
 do.devicecheck=1
 do.modules=0
 do.systemless=1
