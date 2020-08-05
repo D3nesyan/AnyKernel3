bc878d8eb5501845d18c9fee9b690f1bff10f0c1
Ratoriku <a1063021545@gmail.com>
2020-08-05 11:51:14 +0800

    o3t?

--- magisk_module/service.sh
+++ magisk_module/service.sh
@@ -32,13 +32,11 @@ done
 # com.tencent.tmgp.sgame
 File=/data/data/com.tencent.tmgp.sgame/shared_prefs/com.tencent.tmgp.sgame.v2.playerprefs.xml
 
-sed -i '/.*<int name="VulkanTryCount" value=".*" \/>/'d "$File"
 sed -i '/.*<int name="EnableVulkan" value=".*" \/>/'d "$File"
 sed -i '/.*<int name="EnableGLES3" value=".*" \/>/'d "$File"
 sed -i '/.*<int name="EnableMTR" value=".*" \/>/'d "$File"
 sed -i '/.*<int name="DisableMTR" value=".*" \/>/'d "$File"
-sed -i '2a \ \ \ \ <int name="VulkanTryCount" value="1" \/>' "$File";
 sed -i '3a \ \ \ \ <int name="EnableVulkan" value="3" \/>' "$File";
-sed -i '4a \ \ \ \ <int name="EnableGLES3" value="1" \/>' "$File";
+sed -i '4a \ \ \ \ <int name="EnableGLES3" value="2" \/>' "$File";
 sed -i '5a \ \ \ \ <int name="EnableMTR" value="1" \/>' "$File";
 sed -i '6a \ \ \ \ <int name="DisableMTR" value="3" \/>' "$File";
