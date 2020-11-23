c47a62e26a978a0432fbeb387207853c38190efe
Ratoriku <a1063021545@gmail.com>
2020-11-23 22:46:21 +0800

    4.14
    Signed-off-by: Ratoriku <a1063021545@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -37,52 +37,6 @@ split_boot;
 # Clean Up
 patch_cmdline "loop.max_part" "loop.max_part=16"
 
-# Disable MIUI AD
-miui=$(file_getprop /system/build.prop ro.miui.notch);
-if [ "$miui" == "1" ]; then
-rm -rf /data/media/0/miad
-echo "" > /data/media/0/miad
-fi
-
-# Screen OC
-ui_print "Detecting Screen OC..."
-case "$ZIPFILE" in
-  *62fps*|*62hz*)
-    ui_print "Detected! Setting 62 Hz refresh rate..."
-    ui_print "WARNING: Use screen oc AT YOUR RISK"
-    patch_cmdline "mdss_dsi.framerate_override" "mdss_dsi.framerate_override=62"
-    ;;
-  *66fps*|*66hz*)
-    ui_print "Detected! Setting 66 Hz refresh rate..."
-    ui_print "WARNING: Use screen oc AT YOUR RISK"
-    patch_cmdline "mdss_dsi.framerate_override" "mdss_dsi.framerate_override=66"
-    ;;
-  *68fps*|*68hz*)
-    ui_print "Detected! Setting 68 Hz refresh rate..."
-    ui_print "WARNING: Use screen oc AT YOUR RISK"
-    patch_cmdline "mdss_dsi.framerate_override" "mdss_dsi.framerate_override=68"
-    ;;
-  *69fps*|*69hz*)
-    ui_print "Detected! Setting 69 Hz refresh rate..."
-    ui_print "WARNING: Use screen oc AT YOUR RISK"
-    patch_cmdline "mdss_dsi.framerate_override" "mdss_dsi.framerate_override=69"
-    ;;
-  *70fps*|*70hz*)
-    ui_print "Detected! Setting 70 Hz refresh rate..."
-    ui_print "WARNING: Use screen oc AT YOUR RISK"
-    patch_cmdline "mdss_dsi.framerate_override" "mdss_dsi.framerate_override=70"
-    ;;
-  *73fps*|*73hz*)
-    ui_print "Detected! Setting 73 Hz refresh rate..."
-    ui_print "WARNING: Use screen oc AT YOUR RISK"
-    ui_print "You’re using the max refresh rate for lavender currently."
-    patch_cmdline "mdss_dsi.framerate_override" "mdss_dsi.framerate_override=73"
-    ;;
-  *)
-    ui_print "Nothing was Detected! Setting default 60 Hz refresh rate..."
-    patch_cmdline "mdss_dsi.framerate_override" ""
-esac
-
 flash_boot;
 flash_dtbo;
 ## end install
