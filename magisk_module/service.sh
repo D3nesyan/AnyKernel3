e91c126d3efd9864b6975564095ef4f47fe41381
Ratoriku <a1063021545@gmail.com>
2021-02-27 10:12:31 +0800

    update
    Signed-off-by: Ratoriku <a1063021545@gmail.com>

--- magisk_module/service.sh
+++ magisk_module/service.sh
@@ -6,6 +6,17 @@ MODDIR=${0%/*}
 
 # Optimizations
 stop energy-awareness
+miui=$(file_getprop /system/build.prop ro.miui.notch);
 if [ "$miui" == "1" ]; then
 stop miuibooster
 fi
+
+function write() {
+    echo -n $2 > $1
+}
+
+# Use powersave governor for memlat
+for memlat in /sys/class/devfreq/*qcom,memlat-cpu*
+do
+    write $memlat/governor "powersave"
+done
