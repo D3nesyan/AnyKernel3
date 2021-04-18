45576897fa85ab3e7e8e230bb42132fabeacafcb
Ratoriku <a1063021545@gmail.com>
2021-04-18 09:21:42 +0800

    clean up

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=MeowKernel by Ratoriku
+kernel.string=MeowKernel 4.19 by Ratoriku
 do.devicecheck=1
 do.modules=0
 do.systemless=1
@@ -24,59 +24,8 @@ ramdisk_compression=auto;
 # import patching functions/variables - see for reference
 . tools/ak3-core.sh;
 
-# Extra
-ui_print " " "Creating Meow Magisk Module ..."
-rm -rf /data/adb/modules/meow;
-cp -rf $home/magisk_module /data/adb/modules/meow;
-
 ## AnyKernel install
 split_boot;
-
-# Disable MIUI AD
-miui=$(file_getprop /system/build.prop ro.miui.notch);
-if [ "$miui" == "1" ]; then
-rm -rf /data/media/0/miad
-echo "" > /data/media/0/miad
-fi
-
-# Screen OC
-ui_print "Detecting Screen OC..."
-case "$ZIPFILE" in
-  *62fps*|*62hz*)
-    ui_print "Detected! Setting 62 Hz refresh rate..."
-    ui_print "WARNING: Use screen oc AT YOUR RISK"
-    patch_cmdline "mdss_dsi.cur_refresh_rate" "mdss_dsi.cur_refresh_rate=62"
-    ;;
-  *66fps*|*66hz*)
-    ui_print "Detected! Setting 66 Hz refresh rate..."
-    ui_print "WARNING: Use screen oc AT YOUR RISK"
-    patch_cmdline "mdss_dsi.cur_refresh_rate" "mdss_dsi.cur_refresh_rate=66"
-    ;;
-  *68fps*|*68hz*)
-    ui_print "Detected! Setting 68 Hz refresh rate..."
-    ui_print "WARNING: Use screen oc AT YOUR RISK"
-    patch_cmdline "mdss_dsi.cur_refresh_rate" "mdss_dsi.cur_refresh_rate=68"
-    ;;
-  *69fps*|*69hz*)
-    ui_print "Detected! Setting 69 Hz refresh rate..."
-    ui_print "WARNING: Use screen oc AT YOUR RISK"
-    patch_cmdline "mdss_dsi.cur_refresh_rate" "mdss_dsi.cur_refresh_rate=69"
-    ;;
-  *70fps*|*70hz*)
-    ui_print "Detected! Setting 70 Hz refresh rate..."
-    ui_print "WARNING: Use screen oc AT YOUR RISK"
-    patch_cmdline "mdss_dsi.cur_refresh_rate" "mdss_dsi.cur_refresh_rate=70"
-    ;;
-  *73fps*|*73hz*)
-    ui_print "Detected! Setting 73 Hz refresh rate..."
-    ui_print "WARNING: Use screen oc AT YOUR RISK"
-    ui_print "You’re using the max refresh rate for lavender currently."
-    patch_cmdline "mdss_dsi.cur_refresh_rate" "mdss_dsi.cur_refresh_rate=73"
-    ;;
-  *)
-    ui_print "Nothing was Detected! Setting default 60 Hz refresh rate..."
-    patch_cmdline "mdss_dsi.cur_refresh_rate" ""
-esac
 
 flash_boot;
 flash_dtbo;
