3be1160a5572cc0bcd063cf214043718f53058d5
Ratoriku <a1063021545@gmail.com>
2021-06-08 21:27:38 +0800

    update magisk module
    Signed-off-by: Ratoriku <a1063021545@gmail.com>

--- magisk_module/service.sh
+++ magisk_module/service.sh
@@ -3,10 +3,6 @@ MODDIR=${0%/*}
 
 # Optimizations
 stop energy-awareness
-miui=$(file_getprop /system/build.prop ro.miui.notch);
-if [ "$miui" == "1" ]; then
-stop miuibooster
-fi
 
 #function write() {
 #    echo -n $2 > $1
